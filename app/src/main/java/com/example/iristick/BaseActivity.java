b50c2d8fb45aae99f9d7cbf3c1c8a63aeab1e48d
Marius Le Douarin <marius.lledouarin@hotmail.fr>
2023-02-07 13:25:33 +0100

    Fix XML pour les fragments de la caméra, fix des imports dans les différentes classes, fix des erreurs dûes à la sémantique Java, création de la branche courante liée au repo d'Arthur de l'application

---
+++ app/src/main/java/com/example/iristick/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.iristick;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.iristick.smartglass.support.app.IristickApp;
+
+public abstract class BaseActivity extends Activity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(IristickApp.wrapContext(newBase));
+    }
+
+}
---
+++ app/src/main/java/com/example/iristick/Example.java
@@ -0,0 +1,34 @@
+package com.example.iristick;
+
+import androidx.annotation.StringRes;
+
+import com.example.iristick.barcode.BarcodeActivity;
+import com.example.iristick.camera.CameraActivity;
+import com.example.iristick.displaypresentation.DisplayPresentationActivity;
+import com.example.iristick.displaysurface.DisplaySurfaceActivity;
+import com.example.iristick.sensorscube.SensorsCubeActivity;
+import com.example.iristick.sensorsreadout.SensorsReadoutActivity;
+import com.example.iristick.touchpad.TouchpadActivity;
+
+public enum Example {
+    CAMERA(CameraActivity.class, R.string.camera_title, R.string.camera_description),
+    BARCODE(BarcodeActivity.class, R.string.barcode_title, R.string.barcode_description),
+    DISPLAY_PRESENTATION(DisplayPresentationActivity.class, R.string.displaypresentation_title, R.string.displaypresentation_description),
+    DISPLAY_SURFACE(DisplaySurfaceActivity.class, R.string.displaysurface_title, R.string.displaysurface_description),
+    SENSORS_CUBE(SensorsCubeActivity.class, R.string.sensorscube_title, R.string.sensorscube_description),
+    SENSORS_READOUT(SensorsReadoutActivity.class, R.string.sensorsreadout_title, R.string.sensorsreadout_description),
+    TOUCHPAD(TouchpadActivity.class, R.string.touchpad_title, R.string.touchpad_description),
+    ;
+
+    public final Class<? extends BaseActivity> activity;
+    @StringRes
+    public final int title;
+    @StringRes public final int description;
+
+    Example(Class<? extends BaseActivity> activity, int title, int description) {
+        this.activity = activity;
+        this.title = title;
+        this.description = description;
+    }
+
+}
